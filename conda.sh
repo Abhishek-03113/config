2ea20d8adfe7575d99613b2d0ce26a81014758fa
ABHISHEK  PAWAR <97723324+Abhishek-03113@users.noreply.github.com>
2024-02-06 13:48:40 +0530

    Update conda.sh

--- conda.sh
+++ conda.sh
@@ -1,3 +1,4 @@
-curl https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh 
+sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+curl -o https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh 
 chmod +x Anaconda3-2023.09-0-Linux-x86_64.sh 
 bash -b -p $HOME/anaconda3
