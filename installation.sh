6ff367b4166a8e0f06ec874e4c6fb1e90ec6a794
ABHISHEK  PAWAR <97723324+Abhishek-03113@users.noreply.github.com>
2024-02-06 13:30:36 +0530

    Add files via upload

---
+++ installation.sh
@@ -0,0 +1,15 @@
+sudo apt update -y 
+sudo apt install git -y 
+sudo apt install curl -y  
+sudo apt install gcc -y 
+sudo apt install g++ -y 
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget -y 
+sudo apt install python3 -y 
+sudo apt install python3-pip -y 
+apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 -y 
+curl https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh 
+chmod +x Anaconda3-2023.09-0-Linux-x86_64.sh  
+sh Anaconda3-2023.09-0-Linux-x86_64.sh 
+sudo apt install mysql-server -y
+sudo systemctl start mysql.service -y  
+sudo snap install mysql-workbench-community -y 
