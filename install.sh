87ac7836484eda68d5044e020635eac9b53fdced
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2024-02-08 22:24:33 +0530

    snaps and deb packages installed

--- install.sh
+++ install.sh
@@ -7,11 +7,13 @@ sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-
 sudo apt install python3 -y 
 sudo apt install python3-pip -y
 sudo apt install brave-browser 
+sudo apt install flatpak
 
 sudo apt install mysql-server -y
 sudo systemctl start mysql.service
 
 sudo snap install mysql-workbench-community
+
 sudo snap install code --classic
 
 wget https://discord.com/api/download?platform=linux&format=deb 
@@ -20,8 +22,10 @@ sudo dpkg --install discord-*.deb
 wget https://www.google.com/intl/en_in/chrome/next-steps.html?statcb=0&installdataindex=empty&defaultbrowser=0# 
 sudo dpkg --install google-chrome-*.deb 
 
+wget https://downloads.mongodb.com/compass/mongodb-compass_1.42.0_amd64.deb
+sudo dpkg --install mongodb-compass-*.deb 
 
 sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
 wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh 
 chmod +x Anaconda3-2023.09-0-Linux-x86_64.sh 
-bash Anaconda3-2023.09-0-Linux-x86_64.sh    
+bash Anaconda3-2023.09-0-Linux-x86_64.sh 
